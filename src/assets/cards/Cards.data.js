3291e86f093fe80886d94720fe753e55b4db1590
fumiyaO238 <f_otsuka@two-six.jp>
2024-06-19 19:57:23 +0900

    1:06:24

---
+++ src/assets/cards/Cards.data.js
@@ -0,0 +1,53 @@
+import card_img1 from './card1.jpg'
+import card_img2 from './card1.jpg'
+import card_img3 from './card1.jpg'
+import card_img4 from './card1.jpg'
+import card_img5 from './card1.jpg'
+import card_img6 from './card1.jpg'
+import card_img7 from './card1.jpg'
+import card_img8 from './card1.jpg'
+import card_img9 from './card1.jpg'
+import card_img10 from './card1.jpg'
+
+export const cards_data = [
+  {
+    image: card_img1,
+    name: ""
+  },
+  {
+    image: card_img2,
+    name: ""
+  },
+  {
+    image: card_img3,
+    name: ""
+  },
+  {
+    image: card_img4,
+    name: ""
+  },
+  {
+    image: card_img5,
+    name: ""
+  },
+  {
+    image: card_img6,
+    name: ""
+  },
+  {
+    image: card_img7,
+    name: ""
+  },
+  {
+    image: card_img8,
+    name: ""
+  },
+  {
+    image: card_img9,
+    name: ""
+  },
+  {
+    image: card_img10,
+    name: ""
+  },
+]
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,33 @@
 import './Home.css'
 import Navbar from '../../components/Navbar/Navbar'
+import hero_banner from '../../assets/hero_banner.jpg'
+import hero_title from '../../assets/hero_title.jpg'
+import play_icon from '../../assets/play_icon.jpg'
+import info_icon from '../../assets/info_icon.jpg'
+import TitleCards from '../../components/TitleCards/TitleCards'
 
 const Home = () => {
   return (
     <div className='home'>
       <Navbar />
+      <div className="hero">
+        <img src={hero_banner} alt="" className='banner_img' />
+        <div className="hero-caption">
+          <img src={hero_title} alt="" className='caption-img' />
+          <p>Discovering his ties to a secret ancient order, a young man living in modern Istanbul embarks on a quest to save the city from an immortal enemy.</p>
+          <div className='hero-btns'>
+            <button className='btn'><img src={play_icon} alt="" />Play</button>
+            <button className='btn dark-btn'><img src={info_icon} alt="" />More Info</button>
+          </div>
+          <TitleCards />
+        </div>
+      </div>
+      <div className="more-cards">
+        <TitleCards title={"Blockbuster Movies"} />
+        <TitleCards title={"Only on Netflix"} />
+        <TitleCards title={"Upcoming"} />
+        <TitleCards title={"Top Pics for You"} />
+      </div>
     </div>
   )
 }
