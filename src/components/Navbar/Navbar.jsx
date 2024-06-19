6fd29374ca29f5a1c323107740e0324acd2df52f
fumiyaO238 <f_otsuka@two-six.jp>
2024-06-19 17:44:41 +0900

    navbar完成

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,22 +9,26 @@ const Navbar = () => {
   return (
     <div className='navbar'>
       <div className="navbar-left">
-        <img src={logo} style={{width: "20%"}} alt="" />
+        <img src={logo} alt="" />
         <ul>
           <li>Home</li>
           <li>TV Shows</li>
           <li>Movies</li>
           <li>New & Popular</li>
+          <li>My List</li>
           <li>Browse by Language</li>
         </ul>
       </div>
       <div className="navbar-right">
         <img src={serch_icon} alt="" className='icons' />
         <p>Children</p>
-        <img src={bell_icon} style={{width: 16}} alt="" className='icons' />
+        <img src={bell_icon} alt="" className='icons' />
         <div className="navbar-profile">
-          <img src={profile_img} style={{width: 40}} alt="" className='profile' />
+          <img src={profile_img} alt="" className='profile' />
           <img src={caret_icon} style={{width: 16}} alt="" />
+          <div className="dropdown">
+            <p>Sign Out of Netflix</p>
+          </div>
         </div>
       </div>
     </div>
